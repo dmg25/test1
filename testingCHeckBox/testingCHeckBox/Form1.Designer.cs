e4c700f3ab3917fe7ab1461f6dcca6a4a0b6ca63
dmg25 <43114826+dmg25@users.noreply.github.com>
2018-09-17 22:46:41 +0300

    Add files via upload

---
+++ testingCHeckBox/testingCHeckBox/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace testingCHeckBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.checkBox8);
+            this.groupBox1.Controls.Add(this.checkBox7);
+            this.groupBox1.Controls.Add(this.checkBox6);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(556, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(167, 202);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Контейнер с параметрами";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Параметр 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Параметр 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 65);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 17);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "Параметр 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 88);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(86, 17);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "Параметр 4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 111);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(86, 17);
+            this.checkBox5.TabIndex = 0;
+            this.checkBox5.Text = "Параметр 5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(6, 132);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(86, 17);
+            this.checkBox6.TabIndex = 0;
+            this.checkBox6.Text = "Параметр 6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(6, 155);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(86, 17);
+            this.checkBox7.TabIndex = 0;
+            this.checkBox7.Text = "Параметр 7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(6, 178);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(86, 17);
+            this.checkBox8.TabIndex = 0;
+            this.checkBox8.Text = "Параметр 8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(658, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(562, 327);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(110, 17);
+            this.checkBox9.TabIndex = 1;
+            this.checkBox9.Text = "Расклонировать";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(562, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вывести";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 322);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(562, 298);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Размножить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Параметр 1",
+            "Параметр 2",
+            "Параметр 3",
+            "Параметр 4",
+            "Параметр 5",
+            "Параметр 6",
+            "Параметр 7",
+            "Параметр 8"});
+            this.checkedListBox1.Location = new System.Drawing.Point(766, 94);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(146, 199);
+            this.checkedListBox1.TabIndex = 6;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 385);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ testingCHeckBox/testingCHeckBox/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testingCHeckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int pre;
+        public class Boxes
+        {
+            public bool box1;
+            public bool box2;
+            public bool box3;
+            public bool box4;
+            public bool box5;
+            public bool box6;
+            public bool box7;
+            public bool box8;
+            public int index;
+        };
+
+        List<Boxes> BoxList = new List<Boxes>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Объект 1");
+            comboBox1.Items.Add("Объект 2");
+            comboBox1.Items.Add("Объект 3");
+            comboBox1.Items.Add("Объект 4");
+            comboBox1.Items.Add("Объект 5");
+            comboBox1.Items.Add("Объект 6");
+            comboBox1.Items.Add("Объект 7");
+            comboBox1.Items.Add("Объект 8");
+            comboBox1.Items.Add("Объект 9");
+
+            for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                var bx = new Boxes() { index = i };
+
+                BoxList.Add(bx);
+            }
+
+
+
+        }
+
+        private void Clone()
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Объект " + comboBox1.SelectedIndex + 1.ToString();
+
+            int i = comboBox1.SelectedIndex;
+
+
+            BoxList[pre].box1 = checkBox1.Checked;
+            BoxList[pre].box2 = checkBox2.Checked;
+            BoxList[pre].box3 = checkBox3.Checked;
+            BoxList[pre].box4 = checkBox4.Checked;
+            BoxList[pre].box5 = checkBox5.Checked;
+            BoxList[pre].box6 = checkBox6.Checked;
+            BoxList[pre].box7 = checkBox7.Checked;
+            BoxList[pre].box8 = checkBox8.Checked;
+
+            /*BoxList[pre].box1 = checkedListBox1.GetItemChecked(0);
+            BoxList[pre].box2 = checkedListBox1.GetItemChecked(1);
+            BoxList[pre].box3 = checkedListBox1.GetItemChecked(2);
+            BoxList[pre].box4 = checkedListBox1.GetItemChecked(3);
+            BoxList[pre].box5 = checkedListBox1.GetItemChecked(4);
+            BoxList[pre].box6 = checkedListBox1.GetItemChecked(5);
+            BoxList[pre].box7 = checkedListBox1.GetItemChecked(6);
+            BoxList[pre].box8 = checkedListBox1.GetItemChecked(7);*/
+
+           /* checkedListBox1.SetItemChecked(0, BoxList[i].box1);
+            checkedListBox1.SetItemChecked(1, BoxList[i].box2);
+            checkedListBox1.SetItemChecked(2, BoxList[i].box3);
+            checkedListBox1.SetItemChecked(3, BoxList[i].box4);
+            checkedListBox1.SetItemChecked(4, BoxList[i].box5);
+            checkedListBox1.SetItemChecked(5, BoxList[i].box6);
+            checkedListBox1.SetItemChecked(6, BoxList[i].box7);
+            checkedListBox1.SetItemChecked(7, BoxList[i].box8);*/
+
+            
+
+            checkBox1.Checked = BoxList[i].box1;
+            checkBox2.Checked = BoxList[i].box2;
+            checkBox3.Checked = BoxList[i].box3;
+            checkBox4.Checked = BoxList[i].box4;
+            checkBox5.Checked = BoxList[i].box5;
+            checkBox6.Checked = BoxList[i].box6;
+            checkBox7.Checked = BoxList[i].box7;
+            checkBox8.Checked = BoxList[i].box8;
+
+            pre = i;
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            int i = comboBox1.SelectedIndex;
+
+            /*  BoxList[i].box1 = checkBox1.Checked;
+              BoxList[i].box2 = checkBox2.Checked;
+              BoxList[i].box3 = checkBox3.Checked;
+              BoxList[i].box4 = checkBox4.Checked;
+              BoxList[i].box5 = checkBox5.Checked;
+              BoxList[i].box6 = checkBox6.Checked;
+              BoxList[i].box7 = checkBox7.Checked;
+              BoxList[i].box8 = checkBox8.Checked;*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Boxes bx in BoxList)
+            {
+              int i = comboBox1.SelectedIndex;
+
+             /* BoxList[i].box1 = checkedListBox1.GetItemChecked(0);
+              BoxList[i].box2 = checkedListBox1.GetItemChecked(1);
+              BoxList[i].box3 = checkedListBox1.GetItemChecked(2);
+              BoxList[i].box4 = checkedListBox1.GetItemChecked(3);
+              BoxList[i].box5 = checkedListBox1.GetItemChecked(4);
+              BoxList[i].box6 = checkedListBox1.GetItemChecked(5);
+              BoxList[i].box7 = checkedListBox1.GetItemChecked(6);
+              BoxList[i].box8 = checkedListBox1.GetItemChecked(7);*/
+
+              BoxList[i].box1 = checkBox1.Checked;
+              BoxList[i].box2 = checkBox2.Checked;
+              BoxList[i].box3 = checkBox3.Checked;
+              BoxList[i].box4 = checkBox4.Checked;
+              BoxList[i].box5 = checkBox5.Checked;
+              BoxList[i].box6 = checkBox6.Checked;
+              BoxList[i].box7 = checkBox7.Checked;
+              BoxList[i].box8 = checkBox8.Checked;
+
+                textBox1.AppendText("Объект " + bx.index.ToString() + ":\n");
+                textBox1.AppendText("Параметр 1: " + bx.box1.ToString() + ";\n");
+                textBox1.AppendText("Параметр 2: " + bx.box2.ToString() + ";\n");
+                textBox1.AppendText("Параметр 3: " + bx.box3.ToString() + ";\n");
+                textBox1.AppendText("Параметр 4: " + bx.box4.ToString() + ";\n");
+                textBox1.AppendText("Параметр 5: " + bx.box5.ToString() + ";\n");
+                textBox1.AppendText("Параметр 6: " + bx.box6.ToString() + ";\n");
+                textBox1.AppendText("Параметр 7: " + bx.box7.ToString() + ";\n");
+                textBox1.AppendText("Параметр 8: " + bx.box8.ToString() + ";\n");
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (Boxes bs in BoxList)
+            {
+               /* bs.box1 = checkedListBox1.GetItemChecked(0);
+                bs.box2 = checkedListBox1.GetItemChecked(1);
+                bs.box3 = checkedListBox1.GetItemChecked(2);
+                bs.box4 = checkedListBox1.GetItemChecked(3);
+                bs.box5 = checkedListBox1.GetItemChecked(4);
+                bs.box6 = checkedListBox1.GetItemChecked(5);
+                bs.box7 = checkedListBox1.GetItemChecked(6);
+                bs.box8 = checkedListBox1.GetItemChecked(7);*/
+                bs.box1 = checkBox1.Checked;
+                bs.box2 = checkBox2.Checked;
+                bs.box3 = checkBox3.Checked;
+                bs.box4 = checkBox4.Checked;
+                bs.box5 = checkBox5.Checked;
+                bs.box6 = checkBox6.Checked;
+                bs.box7 = checkBox7.Checked;
+                bs.box8 = checkBox8.Checked;
+
+            }
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)   // это плохая идея.
+        {
+            /*  int i = comboBox1.SelectedIndex;
+
+              BoxList[i].box1 = checkedListBox1.GetItemChecked(0);
+              BoxList[i].box2 = checkedListBox1.GetItemChecked(1);
+              BoxList[i].box3 = checkedListBox1.GetItemChecked(2);
+              BoxList[i].box4 = checkedListBox1.GetItemChecked(3);
+              BoxList[i].box5 = checkedListBox1.GetItemChecked(4);
+              BoxList[i].box6 = checkedListBox1.GetItemChecked(5);
+              BoxList[i].box7 = checkedListBox1.GetItemChecked(6);
+              BoxList[i].box8 = checkedListBox1.GetItemChecked(7);*/
+
+
+            // попробовать производить запись по изменению листа и по кнопке вывести соответственно (какой лист выбран, тот и сохраняем.)
+
+
+
+        }
+    }
+}
